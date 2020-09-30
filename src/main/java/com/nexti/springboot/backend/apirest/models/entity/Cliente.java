5775d82bf0d8ea5147027165582fe93bcd600c30
juantonguino <juan.tonguino@gmail.com>
2020-09-29 21:39:07 -0500

    initial commit

---
+++ src/main/java/com/nexti/springboot/backend/apirest/models/entity/Cliente.java
@@ -0,0 +1,92 @@
+package com.nexti.springboot.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="clientes")
+public class Cliente implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@NotEmpty(message = "no puede ser vacío") 
+	@Size(min=4 , max=12, message = "el tamaño debe ser mayor a 4 y menor a 12 caracteres")
+	@Column(nullable=false)
+	private String nombre;
+	
+	@NotEmpty(message = "no puede ser vacío")  
+	private String apellido;
+	
+	
+	@NotEmpty(message = "no puede ser vacío")  
+	@Email(message = "no tiene un formato de correo valido")  
+	@Column(nullable=false,unique=true)
+	private String email;
+	
+	@Column(name="create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+
+	@PrePersist
+	public void prePersist() {
+		createAt = new Date();
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/nexti/springboot/backend/apirest/models/servicies/ClienteServiceImpl.java
@@ -0,0 +1,53 @@
+package com.nexti.springboot.backend.apirest.models.servicies;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.nexti.springboot.backend.apirest.models.dao.IClienteDao;
+import com.nexti.springboot.backend.apirest.models.entity.Cliente;
+
+@Service
+public class ClienteServiceImpl implements IClienteService {
+	@Autowired
+	private IClienteDao clienteDao;
+	@Override
+	@Transactional(readOnly = true)
+	public List<Cliente> findAll() {
+
+		return (List<Cliente>) clienteDao.findAll();
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Cliente> findAll(Pageable pageable) {
+		return clienteDao.findAll(pageable);
+	}
+	
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Cliente findById(Long id) {
+	
+		return clienteDao.findById(id).orElse(null);
+	}
+	@Override
+	@Transactional
+	public Cliente save(Cliente cliente) {
+	
+		return clienteDao.save(cliente);
+	}
+	@Override
+	@Transactional
+	public void delete(long id) {
+		clienteDao.deleteById(id);
+	}
+
+	
+	
+
+}
